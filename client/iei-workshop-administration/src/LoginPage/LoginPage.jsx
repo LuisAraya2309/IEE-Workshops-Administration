be6f2fd62d9f8a82780fb3adea999cdd2bb8bc6a
Daniel Cascante <cascantekevin374@gmail.com>
2023-03-22 17:25:13 -0600

    Login Page TESTCREATION

---
+++ client/iei-workshop-administration/src/LoginPage/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React,{Fragment} from 'react'
+
+export function LoginPage() {
+  return (
+    <Fragment>
+    <div>Iniciar Sesion</div>
+    <div>Bienvenido a la pagina</div>
+    </Fragment>
+  )
+}
