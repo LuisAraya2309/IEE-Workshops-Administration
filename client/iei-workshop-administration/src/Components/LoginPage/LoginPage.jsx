013608ab609065ad348b1fc2c0d655758f47a581
Daniel Cascante <cascantekevin374@gmail.com>
2023-03-23 21:27:18 -0600

    LoginPage BASE

--- client/iei-workshop-administration/src/Components/LoginPage/LoginPage.jsx
+++ client/iei-workshop-administration/src/Components/LoginPage/LoginPage.jsx
@@ -1,13 +1,11 @@
 import React,{Fragment, useState} from 'react'
 
-
-function LoginPage() {
+export function LoginPage () {
   const [email, setEmail]=useState('')
   const [password, setPassword]=useState('')
 
   async function submit(e){
     e.preventDefault();
-
     try {
       console.log(e)
     } catch (e) {
@@ -17,18 +15,16 @@ function LoginPage() {
 
   return (
     <Fragment>
-    <div className='login'>
+    <div className='LoginPage'>
       <h1>Iniciar Sesion</h1>
 
       <form action="POST">
         <input type="text" onChange={(e)=>{setEmail(e.target.value)}} placeholder="Email" name="" id=""/>
         <input type="text" onChange={(e)=>{setPassword(e.target.value)}} placeholder="Password" name="" id=""/>
 
-        <input type="submit" onClick={submit}/>
+        <button type="submit" onClick={submit}/>
       </form>
     </div>
     </Fragment>
   )
 }
-
-export default LoginPage
