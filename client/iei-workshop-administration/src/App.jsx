283977e3095d8f942e3ca6cf79926fc2a42d86d3
LuisAraya2309 <luistec2309@estudiantec.cr>
2023-03-27 22:46:28 -0600

    React versions bugged

--- client/iei-workshop-administration/src/App.jsx
+++ client/iei-workshop-administration/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { LoginPage } from './Components/LoginPage/LoginPage';
+import {AdminPage} from './Components/AdminPage/AdminPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export function App () {
@@ -9,6 +10,7 @@ export function App () {
       <Router>
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/AdminPage" element={<AdminPage />} />
         </Routes>
       </Router>
     </div>
