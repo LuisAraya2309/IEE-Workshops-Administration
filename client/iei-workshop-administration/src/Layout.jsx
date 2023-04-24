cf3a2a7fc1f22ac97d8b58bef9a0ab1979fabe90
SebasDiaz01182 <sebastian.diaz@estudiantec.cr>
2023-04-24 09:50:33 -0600

    Layout ready

--- client/iei-workshop-administration/src/Layout.jsx
+++ client/iei-workshop-administration/src/Layout.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import circle from "./resources/image.png"
 import './Layout.css';
 
 const Layout = ({ children }) => {
   return (
     <div className = "Layout">
-        <div class="image-container">
-            <img src = {circle} alt="Layout"/>
-        </div>
         {children}
     </div>
   );
--- client/iei-workshop-administration/src/pages/AdminPage/AdminPage.jsx
+++ client/iei-workshop-administration/src/pages/AdminPage/AdminPage.jsx
@@ -1,16 +1,106 @@
+import Layout from '../../Layout';
 import * as React from 'react';
+import { useLocation } from 'react-router-dom';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import Layout from '../../Layout';
+import { AppBar, Toolbar, Typography, Box, Container } from '@mui/material'
+import { useNavigate } from 'react-router-dom';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+import EditIcon from '@mui/icons-material/Edit';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 export  function AdminPage() {
+  //const {state} = useLocation();
+  const userLogged = "" //state.user
+
+  let navigate = useNavigate()
+  const salirALogIn = () =>{
+    let logInPath = "/"
+    navigate(logInPath)
+  }
+
+  const irAFormularios = () =>{
+    /*let formsPath = "/"
+    navigate(formsPath, {state:{user:userLogged}}) // acá se pasa el email*/
+    alert("Va a crear formularios")
+  }
+
+  const irAMetricas = () =>{
+    /*let metricasPath = "/"
+    navigate(metricasPath, {state:{user:userLogged}}) // acá se pasa el email*/
+    alert("Va a ver las metricas")
+  }
+
+  const irAEditarTalleres = () =>{
+    /*let talleresPath = "/"
+    navigate(talleresPath, {state:{user:userLogged}}) // acá se pasa el email*/
+    alert("Va a editar los talleres")
+  }
+
+  const irAagregarUsuarios = () =>{
+    /*let talleresPath = "/"
+    navigate(talleresPath, {state:{user:userLogged}}) // acá se pasa el email*/
+    alert("Va a agregar usuarios")
+  }
+
   return (
     <Layout>
-      <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
+    <Stack spacing={4}>
+      <Stack
+        sx={{ pt: 4 }}
+        direction="row"
+        spacing={4}
+        justifyContent="center"
+      >
+        <Box sx={{ '& button': { m: 1 } }}>
+          <br /> <br /> <br />
+          <div>
+            <Button
+              startIcon={<AddBoxIcon />}
+              size="large"
+              color="success"
+              variant="contained" 
+              onClick={irAFormularios}>
+                Nuevo Formulario
+            </Button>
+          </div>
+          <br />
+          <div>
+            <Button 
+              size="large"
+              startIcon={<LeaderboardIcon />}
+              color="success"
+              variant="outlined"
+              onClick={irAMetricas}>
+                Análisis de información
+            </Button>
+          </div>
+          <br />
+          <div>
+            <Button 
+              size="large"
+              startIcon={<EditIcon />}
+              color="success"
+              variant="contained"
+              onClick={irAEditarTalleres}>
+                Modulo de gestión
+            </Button>
+          </div>
+          <br />
+          <div>
+            <Button 
+              size="large"
+              startIcon={<PersonAddIcon />}
+              color="success"
+              variant="outlined"
+              onClick={irAagregarUsuarios}>
+                Registrar un usuario    
+            </Button>
+          </div>
+        </Box>
       </Stack>
+    </Stack>
     </Layout>
   );
 }
